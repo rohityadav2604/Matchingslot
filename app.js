631fc9d271401e10fbb3552d70657d3473273b2e
Rohit <rohityadav2604@gmail.com>
2021-10-27 10:24:10 +0530

    added mongoose+route

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const user = {
 
+}
 
 app.get('/' , (req , res)=>{
     res.send("basic server");
 })
+
+app.post('/slotbook' , (req , res)=>{
+    //save object;
+})
+mongoose.connect("mongodb+srv://Matching:<admin@123>@cluster0.jabnw.mongodb.net/Matching?retryWrites=true&w=majority")
 app.listen(3000);
